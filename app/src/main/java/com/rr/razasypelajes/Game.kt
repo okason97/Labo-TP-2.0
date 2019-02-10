f0f8e7fb2a5d5608f12566e92c3a521e0d9cdc65
okason <okason1997@hotmail.com>
2019-02-10 01:12:51 -0300

    game finished??

--- app/src/main/java/com/rr/razasypelajes/Game.kt
+++ app/src/main/java/com/rr/razasypelajes/Game.kt
@@ -2,6 +2,7 @@ package com.rr.razasypelajes
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.drawable.AnimationDrawable
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
@@ -11,8 +12,25 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlin.collections.ArrayList
 import kotlin.properties.Delegates
+import android.widget.ImageView
 
-class Game: AppCompatActivity(), ExitDialog.ExitDialogListener {
+
+
+class Game: AppCompatActivity(), ExitDialog.ExitDialogListener, VictoryDialog.VictoryDialogListener {
+    override fun onVictoryDialogNegativeClick(dialog: DialogFragment) {
+        val sharedPref = getSharedPreferences(getString(R.string.config), Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = sharedPref.edit()
+        editor.putInt(getString(R.string.minijuego),
+                R.id.razasYPelajes)
+        editor.putBoolean(getString(R.string.level), false)
+        editor.apply()
+        setGameMode(sharedPref)
+        gameMode.newGame()
+    }
+
+    override fun onVictoryDialogPositiveClick(dialog: DialogFragment) {
+        finish()
+    }
 
     override fun onExitDialogPositiveClick(dialog: DialogFragment) {
         finish()
@@ -94,15 +112,54 @@ class Game: AppCompatActivity(), ExitDialog.ExitDialogListener {
     fun selectedAnswer(view: View) {
         if (view.id == answer) {
             victories++
-            playCorrect()
             if (count < 5) {
+                playCorrect()
                 Handler().postDelayed({
                     gameMode.newGame()
                 }, correctDuration())
             }else{
                 if (victories >= 3){
                     val sharedPref = getSharedPreferences(getString(R.string.config), Context.MODE_PRIVATE)
-                    if ()
+                    val editor : SharedPreferences.Editor
+                    if (sharedPref.getBoolean(getString(R.string.level), false)){
+                        when (sharedPref.getInt(getString(R.string.minijuego),
+                                R.id.razasYPelajes)){
+                            R.id.razasYPelajes -> {
+                                playNextLevel()
+                                Handler().postDelayed({
+                                    setGameMode(sharedPref)
+                                    gameMode.newGame()
+                                }, nextLevelDuration())
+                                editor = sharedPref.edit()
+                                editor.putInt(getString(R.string.minijuego),
+                                        R.id.razasYPelajesJuntas)
+                                editor.putBoolean(getString(R.string.level), false)
+                                editor.apply()
+                            }
+                            R.id.razasYPelajesJuntas -> {
+                                playNextLevel()
+                                Handler().postDelayed({
+                                    setGameMode(sharedPref)
+                                    gameMode.newGame()
+                                }, nextLevelDuration())
+                                editor = sharedPref.edit()
+                                editor.putInt(getString(R.string.minijuego),
+                                        R.id.cruzas)
+                                editor.putBoolean(getString(R.string.level), false)
+                                editor.apply()
+                            }
+                            R.id.cruzas -> {
+                                playVictory()
+                                Handler().postDelayed({
+                                    victoryDialog()
+                                }, victoryDuration())
+                            }
+                        }
+                    }else{
+                        editor = sharedPref.edit()
+                        editor.putBoolean(getString(R.string.level), true)
+                        editor.apply()
+                    }
                 }
             }
         } else {
@@ -110,6 +167,36 @@ class Game: AppCompatActivity(), ExitDialog.ExitDialogListener {
             playError()
         }
         count++
+    }
+
+    private fun nextLevelDuration(): Long {
+        val frameDuration : Long = 25
+        val framesSize : Long = 71
+        return frameDuration*framesSize
+    }
+
+    private fun playNextLevel() {
+        val img = findViewById<ImageView>(R.id.confeti_anim)
+        val anim : AnimationDrawable = img.drawable as AnimationDrawable
+        img.post { anim.start() }
+    }
+
+    private fun victoryDialog() {
+        // Create an instance of the dialog fragment and show it
+        val dialog : DialogFragment = VictoryDialog()
+        dialog.show(supportFragmentManager, "NoticeDialogFragment")
+    }
+
+    private fun victoryDuration(): Long {
+        val frameDuration : Long = 25
+        val framesSize : Long = 80
+        return frameDuration*framesSize
+    }
+
+    private fun playVictory() {
+        val img = findViewById<ImageView>(R.id.cup_anim)
+        val anim : AnimationDrawable = img.drawable as AnimationDrawable
+        img.post { anim.start() }
     }
 
     private fun restartError() {
@@ -134,9 +221,9 @@ class Game: AppCompatActivity(), ExitDialog.ExitDialogListener {
 
     private fun playCorrect() = playSound(getString(R.string.correct_sound_key))
 
-    private fun goBack(view: View?){
+    fun goBack(view: View?){
         // Create an instance of the dialog fragment and show it
-        val dialog : DialogFragment = ExitDialog();
-        dialog.show(supportFragmentManager, "NoticeDialogFragment");
+        val dialog : DialogFragment = ExitDialog()
+        dialog.show(supportFragmentManager, "NoticeDialogFragment")
     }
 }
---
+++ app/src/main/java/com/rr/razasypelajes/VictoryDialog.kt
@@ -0,0 +1,50 @@
+package com.rr.razasypelajes
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+
+class VictoryDialog : DialogFragment() {
+    /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+
+    interface VictoryDialogListener {
+        fun onVictoryDialogPositiveClick(dialog : DialogFragment)
+        fun onVictoryDialogNegativeClick(dialog : DialogFragment)
+    }
+
+    // Use this instance of the interface to deliver action events
+    private lateinit var mListener : VictoryDialogListener
+
+    // Override the Fragment.onAttach() method to instantiate the ExitDialogListener
+    override fun onAttach(context : Context) {
+        super.onAttach(context)
+        // Verify that the host context implements the callback interface
+        try {
+            // Instantiate the ExitDialogListener so we can send events to the host
+            mListener = context as VictoryDialogListener
+        } catch (e : ClassCastException) {
+            // The context doesn't implement the interface, throw exception
+            throw ClassCastException(context.toString()
+                    + " must implement ExitDialogListener")
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Build the dialog and set up the button click handlers
+        val builder : AlertDialog.Builder = AlertDialog.Builder(activity)
+        builder.setMessage(R.string.victory_message)
+                .setPositiveButton(R.string.victory_finish) { _, _ ->
+                    // Send the positive button event back to the host activity
+                    mListener.onVictoryDialogPositiveClick(this@VictoryDialog)
+                }
+                .setNegativeButton(R.string.victory_restart) { _, _ ->
+                    // Send the negative button event back to the host activity
+                    mListener.onVictoryDialogNegativeClick(this@VictoryDialog)
+                }
+        return builder.create()
+    }
+}
