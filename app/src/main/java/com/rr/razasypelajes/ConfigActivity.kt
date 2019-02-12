8288324c59708d12c04db3e1e3e633ddaf10cf72
okason <okason1997@hotmail.com>
2019-02-12 02:25:15 -0300

    algo

--- app/src/main/java/com/rr/razasypelajes/ConfigActivity.kt
+++ app/src/main/java/com/rr/razasypelajes/ConfigActivity.kt
@@ -9,25 +9,23 @@ import android.view.View
 import android.widget.CheckBox
 import android.widget.RadioGroup
 import android.widget.Switch
-import java.util.HashSet
 
 class ConfigActivity : AppCompatActivity(), ExitDialog.ExitDialogListener {
-    var preferences = getSharedPreferences(getString(R.string.config), Context.MODE_PRIVATE) as SharedPreferences
-    var editor = preferences.edit() as SharedPreferences.Editor
+    private val preferences : SharedPreferences = getSharedPreferences(getString(R.string.config), Context.MODE_PRIVATE)
 
     // Recon
-    var reconRadio = findViewById<RadioGroup>(R.id.settingsReconGroup)
-    var reconRyP = findViewById<CheckBox>(R.id.settingsReconRyP)
-    var reconcruza = findViewById<CheckBox>(R.id.settingsReconCruza)
+    private val reconRadio : RadioGroup = findViewById(R.id.settingsReconGroup)
+    private val reconRyP : CheckBox = findViewById(R.id.settingsReconRyP)
+    private val reconCruza: CheckBox = findViewById(R.id.settingsReconCruza)
 
     // Minigame
-    var minigameRadio = findViewById<RadioGroup>(R.id.settingsMinigameGroup)
+    private val minigameRadio : RadioGroup = findViewById(R.id.settingsMinigameGroup)
 
     // Dificulty
-    var dificultySwitch = findViewById<Switch>(R.id.settingsDificultySwitch)
+    private val dificultySwitch: Switch = findViewById(R.id.settingsDificultySwitch)
 
     // Audio
-    var audioSwitch = findViewById<Switch>(R.id.settingsAudioSwitch)
+    private val audioSwitch : Switch = findViewById(R.id.settingsAudioSwitch)
 
     override fun onExitDialogPositiveClick(dialog: DialogFragment) {
         finish()
@@ -52,22 +50,88 @@ class ConfigActivity : AppCompatActivity(), ExitDialog.ExitDialogListener {
 
     private fun loadValues() {
         // Recon
-        reconRadio.check(preferences.getInt("reconRadio", R.id.lista))
-        reconRyP.isChecked = preferences.getBoolean("reconRyP", false)
-        reconcruza.isChecked = preferences.getBoolean("reconRyP", false)
-        if (!(reconRyP.isChecked && reconcruza.isChecked)) reconRyP.isChecked = true
+        reconRadio.check(preferences.getInt(getString(R.string.reconRadio),
+                R.id.lista))
+        reconRyP.isChecked = preferences.getBoolean(getString(R.string.reconRyP),
+                false)
+        reconCruza.isChecked = preferences.getBoolean(getString(R.string.reconRyP),
+                false)
+        if (!(reconRyP.isChecked && reconCruza.isChecked)) reconRyP.isChecked = true
 
         // Minigame
-        minigameRadio.check(preferences.getInt("minigameRadio", R.id.rypImagenPalabra))
+        minigameRadio.check(preferences.getInt(getString(R.string.minijuego),
+                R.id.rypImagenPalabra))
 
         // Dificulty
-        dificultySwitch.isChecked = preferences.getBoolean("dificultySwitch", false)
+        dificultySwitch.isChecked = preferences.getBoolean(
+                getString(R.string.dificultySwitch), false)
 
         // Audio
-        audioSwitch.isChecked = preferences.getBoolean("audioSwitch", false)
+        audioSwitch.isChecked = preferences.getBoolean(getString(R.string.audioSwitch),
+                false)
+
+        val currentMaxLevel = preferences.getInt(getString(R.string.lastLevel), 1)
+        when(preferences.getInt(getString(R.string.minijuego), R.id.razasYPelajes)){
+            R.id.rypImagenPalabra -> {
+                if(currentMaxLevel > resources.getInteger(
+                                R.integer.firstRyPImagenPalabraLevel)){
+                    dificultySwitch.isEnabled = true
+                    dificultySwitch.isClickable = true
+                }
+            }
+            R.id.rypPalabraImagen -> {
+                if(currentMaxLevel > resources.getInteger(
+                                R.integer.firstRyPPalabraImagenLevel)){
+                    dificultySwitch.isEnabled = true
+                    dificultySwitch.isClickable = true
+                }
+            }
+            R.id.cruzas -> {
+                if(currentMaxLevel > resources.getInteger(R.integer.firstCruzasLevel)){
+                    dificultySwitch.isEnabled = true
+                    dificultySwitch.isClickable = true
+                }
+            }
+        }
     }
 
     private fun saveValues() {
-
+        val editor : SharedPreferences.Editor = preferences.edit()
+        editor.apply()
     }
+
+    fun onChangeMinigame(view : View){
+        val currentMaxLevel = preferences.getInt(getString(R.string.lastLevel), 1)
+        when(view.id){
+            R.id.rypImagenPalabra -> {
+                if(currentMaxLevel > resources.getInteger(R.integer.firstRyPImagenPalabraLevel)){
+                    dificultySwitch.isEnabled = true
+                    dificultySwitch.isClickable = true
+                }else{
+                    dificultySwitch.isEnabled = false
+                    dificultySwitch.isClickable = false
+                }
+            }
+            R.id.rypPalabraImagen -> {
+                if(currentMaxLevel > resources.getInteger(R.integer.firstRyPPalabraImagenLevel)){
+                    dificultySwitch.isEnabled = true
+                    dificultySwitch.isClickable = true
+                }else{
+                    dificultySwitch.isEnabled = false
+                    dificultySwitch.isClickable = false
+                }
+            }
+            R.id.cruzas -> {
+                if(currentMaxLevel > resources.getInteger(R.integer.firstCruzasLevel)){
+                    dificultySwitch.isEnabled = true
+                    dificultySwitch.isClickable = true
+                }else{
+                    dificultySwitch.isEnabled = false
+                    dificultySwitch.isClickable = false
+                }
+            }
+        }
+    }
+
+
 }
