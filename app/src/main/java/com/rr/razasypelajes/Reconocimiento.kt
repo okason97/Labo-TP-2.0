abd4c227de5a1514118585f6f4df8e908283d802
Sebastian Raimondi <sebastianraimondi@gmail.com>
2019-02-14 03:01:09 -0300

    Set relincho on missing horse sound, Fix horse type to load

--- app/src/main/java/com/rr/razasypelajes/Reconocimiento.kt
+++ app/src/main/java/com/rr/razasypelajes/Reconocimiento.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.CheckBox
 import com.rr.razasypelajes.helpers.JSONHelper
 import com.rr.razasypelajes.horses.Horse
 
@@ -19,6 +20,7 @@ class Reconocimiento : AppCompatActivity(), DialogExit.ExitDialogListener {
 
     val horses : ArrayList<Horse> = ArrayList()
     val sounds : ArrayList<MediaPlayer> = ArrayList()
+    private val sharedPref : SharedPreferences = getSharedPreferences(getString(R.string.config), Context.MODE_PRIVATE)
 
     private fun loadRazasYPelajes() {
         val hCol = JSONHelper.fromJSON(Horse::class.java, resources.openRawResource(R.raw.horses))
@@ -27,7 +29,9 @@ class Reconocimiento : AppCompatActivity(), DialogExit.ExitDialogListener {
             try {
                 val id = resources.getIdentifier("sound_" + h.raza, "raw", packageName)
                 sounds.add(MediaPlayer.create(this, id))
-            } catch (e: Exception) { println("Horse: $h. E: $e") }
+            } catch (e: Exception) {
+                sounds.add(MediaPlayer.create(this, R.raw.sound_relincho))
+            }
         }
     }
 
@@ -36,26 +40,25 @@ class Reconocimiento : AppCompatActivity(), DialogExit.ExitDialogListener {
         // lo mismo pero con .cruza
     }
 
-    private fun initializeHorses(sharedPref: SharedPreferences) {
-        val defaultRecon : HashSet<String> = HashSet()
-        defaultRecon.add(getString(R.string.reconRyP))
+    private fun initializeHorses() {
+        var ryp = sharedPref.getBoolean(getString(R.string.reconRyP), false)
+        val cruzas = sharedPref.getBoolean(getString(R.string.reconCruza), false)
 
-        val gamemodes  = sharedPref.getStringSet(getString(R.string.gameMode), defaultRecon)
+        if (!ryp && !cruzas) ryp = true
 
-        if (gamemodes.contains(getString(R.string.reconRyP))) loadRazasYPelajes()
-        if (gamemodes.contains(getString(R.string.reconCruza))) loadCruzas()
+        if (ryp) loadRazasYPelajes()
+        if (cruzas) loadCruzas()
     }
 
-    private fun setReconMode(sharedPref: SharedPreferences){
+    private fun setReconMode(){
         val viewMode = sharedPref.getInt(getString(R.string.reconRadio), R.id.lista)
         mode = if (viewMode == R.id.lista) ReconLista(this) else ReconGrilla(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val sharedPref = getSharedPreferences(getString(R.string.config), Context.MODE_PRIVATE)
-        initializeHorses(sharedPref)
-        setReconMode(sharedPref)
+        initializeHorses()
+        setReconMode()
         mode.runRecon()
     }
 
