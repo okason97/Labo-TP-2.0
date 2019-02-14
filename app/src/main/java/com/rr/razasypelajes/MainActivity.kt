7de925da8658e9085398173d36c71f2fad872380
Sebastian Raimondi <sebastianraimondi@gmail.com>
2019-02-14 03:40:58 -0300

    Add AboutDialog

--- app/src/main/java/com/rr/razasypelajes/MainActivity.kt
+++ app/src/main/java/com/rr/razasypelajes/MainActivity.kt
@@ -3,10 +3,13 @@ package com.rr.razasypelajes
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DialogAbout.Listener {
+    override fun onAboutDialogPositiveClick(dialog: DialogFragment) {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,4 +30,11 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, Reconocimiento::class.java)
         startActivity(intent)
     }
+
+    fun onAbout(view: View?){
+        // Create an instance of the dialog fragment and show it
+        val dialog : DialogFragment = DialogAbout()
+        dialog.show(supportFragmentManager, "NoticeDialogFragment")
+    }
+
 }
